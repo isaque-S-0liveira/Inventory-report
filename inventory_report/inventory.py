1842f88b61081f36bcb06cb8e9dc120262e4f425
Isaque <zazac3179@gmail.com>
2023-08-25 17:07:14 -0300

    implementação da classe Inventory

--- inventory_report/inventory.py
+++ inventory_report/inventory.py
@@ -1,2 +1,14 @@
+from inventory_report.product import Product
+from typing import List
+
+
 class Inventory:
-    pass
+    def __init__(self, data: List[Product] | None = None):
+        self._data = [] if data is None else data
+
+    @property
+    def data(self) -> List[Product]:
+        return self._data.copy()
+
+    def add_data(self, data: List[Product]) -> None:
+        self._data.extend(data)
