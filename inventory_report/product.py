21ba949405c7ee00ff46cfafb4fa14e5f9413445
Isaque <zazac3179@gmail.com>
2023-08-24 22:46:04 -0300

    implementação dos testes da classe Product + correção no construtor da classe Product

--- inventory_report/product.py
+++ inventory_report/product.py
@@ -3,13 +3,23 @@ from dataclasses import dataclass
 
 @dataclass
 class Product:
-    id: str
-    product_name: str
-    company_name: str
-    manufacturing_date: str
-    expiration_date: str
-    serial_number: str
-    storage_instructions: str
+    def __init__(
+        self,
+        id: str,
+        product_name: str,
+        company_name: str,
+        manufacturing_date: str,
+        expiration_date: str,
+        serial_number: str,
+        storage_instructions: str,
+    ) -> None:
+        self.id = id
+        self.product_name = product_name
+        self.company_name = company_name
+        self.manufacturing_date = manufacturing_date
+        self.expiration_date = expiration_date
+        self.serial_number = serial_number
+        self.storage_instructions = storage_instructions
 
     def __str__(self) -> str:
         return (
--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,2 +1,12 @@
+from inventory_report.product import Product
+
+
 def test_create_product() -> None:
-    raise NotImplementedError
+    product = Product("1", "Arroz", "KG", "5", "1", "0", "cool dry place")
+    assert product.id == "1"
+    assert product.product_name == "Arroz"
+    assert product.company_name == "KG"
+    assert product.manufacturing_date == "5"
+    assert product.expiration_date == "1"
+    assert product.serial_number == "0"
+    assert product.storage_instructions == "cool dry place"
