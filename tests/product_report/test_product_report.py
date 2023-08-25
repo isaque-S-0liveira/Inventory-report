b6b6e6feb1eb7bb38157fb9905cf1b54e57567af
Isaque <zazac3179@gmail.com>
2023-08-24 23:05:34 -0300

    implementação do teste para classe Product, que testa se o "método mágico" str retorna a frase correta

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,2 +1,10 @@
+from inventory_report.product import Product
+
+
 def test_product_report() -> None:
-    raise NotImplementedError
+    product = Product("1", "Arroz", "KG", "5", "1", "0", "cool dry place")
+    assert str(product) == (
+        "The product 1 - Arroz with serial number 0 manufactured on 5 by the "
+        "company KG valid until 1 must be stored according to the following "
+        "instructions: cool dry place."
+    )
