5832a9442b6552532f4dd203d195ee7610726328
Isaque <zazac3179@gmail.com>
2023-08-25 17:23:12 -0300

    implementação do protocolo Report

--- inventory_report/reports/report.py
+++ inventory_report/reports/report.py
@@ -1,2 +1,11 @@
-class Report:
-    pass
+from inventory_report.inventory import Inventory
+from typing import Protocol
+
+
+# protocolo Report
+class Report(Protocol):
+    def add_inventory(self, inventory: Inventory) -> None:
+        pass
+
+    def generate(self) -> str:
+        pass
